c87470375714edd7465e4026c9ea789963c97471
philheb <tom@toms-MacBook-Pro.local>
2019-03-06 04:29:58 -0800

    Fixed the warehouses UI

--- front-end/src/Components/Warehouse/Warehouse.js
+++ front-end/src/Components/Warehouse/Warehouse.js
@@ -1,29 +1,34 @@
 import React from 'react'
+import rightArrow from '../../Assets/Icons/Icon-arrow-right.svg'
 
 export default function Warehouse(props) {
   return (
-    <tr>
-
-
-      <td data-label="WAREHOUSE">
-
-        {props.name}
-        <br />
-        <p className="description__font">{props.address}</p>
-      </td>
-      <td data-label="CONTACT">
-
-        {props.managerName}
-        <br />
-        {props.managerTitle}
-      </td>
-
-      <td data-label="CONTACT INFORMATION">
-        {props.phone}
-        <br />
-        {props.email}
-      </td>
-      <td data-label="CATEGORIES">{props.categories}</td>
-    </tr>
+    <>
+      <div className="location">
+        <div className="location__details">
+          <div className="location__warehouse location__section">
+            <h4 id="location__name">{props.name}</h4>
+            <h4 className="location__address">{props.address}</h4>
+          </div>
+          <div className="location__bottom">
+            <div className="location__contact location__section">
+              <h4>{props.managerName}</h4>
+              <h4 id="location__manager--title">{props.managerTitle}</h4>
+            </div>
+            <div className="location__contact--info location__section">
+              <h4>{props.phone}</h4>
+              <h4>{props.email}</h4>
+            </div>
+            <div className="location__categories location__section">
+              <h4>{props.categories}</h4>
+            </div>
+          </div>
+        </div>
+        <div className="location__arrow">
+          <img src={rightArrow} alt="arrow-right" />
+        </div>
+      </div>
+      <hr className="location__hr" />
+    </>
   )
 }
