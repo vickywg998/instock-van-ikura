f79d60622b2928b74ee5f46355c93c88b3f93c15
philheb <tom@android-c9550bb7b6ec7fbc.localdomain>
2019-03-06 13:08:48 -0800

    Added Modal and Backdrop for adding warehouse

---
+++ front-end/src/Components/UI/Modal/Modal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Backdrop from '../Backdrop/Backdrop'
+import './Modal.scss'
+
+const Modal = props => (
+  <>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      className="modal"
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0',
+      }}
+    >
+      {props.children}
+    </div>
+  </>
+)
+
+export default Modal
--- front-end/src/Components/Warehouses/Warehouses.js
+++ front-end/src/Components/Warehouses/Warehouses.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Navbar from '../Navbar/Navbar'
 import Warehouse from '../Warehouse/Warehouse'
+import Modal from '../UI/Modal/Modal'
+import addIcon from '../../Assets/Icons/Icon-add.svg'
 import './Warehouses.scss'
 
 class Warehouses extends Component {
   state = {
+    adding: false,
     data: [
       {
         name: 'Warehouse Number 1',
@@ -99,6 +102,16 @@ class Warehouses extends Component {
     ],
   }
 
+  handleAddingLocation = () => {
+    this.setState({ adding: true })
+    console.log(this.state.adding)
+  }
+
+  handleCancelAddingLocation = () => {
+    this.setState({ adding: false })
+    console.log(this.state.adding)
+  }
+
   render() {
     let rows = this.state.data.map((warehouse, i) => {
       return (
@@ -116,6 +129,10 @@ class Warehouses extends Component {
     })
     return (
       <>
+        <Modal
+          show={this.state.adding}
+          closeModal={this.handleCancelAddingLocation}
+        />
         <Navbar routeProps={this.props.routeProps} />
         <div className="location__container">
           <div className="location__header">
@@ -123,6 +140,14 @@ class Warehouses extends Component {
             <input type="text" placeholder="Search" className="search" />
           </div>
           <div className="locations">{rows}</div>
+          <div className="location__add btn-add">
+            <img
+              className="location__add--btn"
+              src={addIcon}
+              alt="add-button"
+              onClick={this.handleAddingLocation}
+            />
+          </div>
         </div>
       </>
     )
