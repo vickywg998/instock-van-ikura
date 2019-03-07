b047628cc2ad8c7ef51dbe3a4d3cd237490a2d2d
philheb <tom@android-c9550bb7b6ec7fbc.localdomain>
2019-03-06 17:37:37 -0800

    Added DELETE item

--- back-end/routes/inventory.js
+++ back-end/routes/inventory.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const uuid = require('uuid')
 const inventoryList = require('../data/inventory.json')
 const INVENTORY__DATA__FILE = './data/inventory.json'
 const fs = require('fs')
 router.use(bodyParser.json())
-router.use(express.static('website'));
-router.use(bodyParser.urlencoded({ extended: false }));
+router.use(express.static('website'))
+router.use(bodyParser.urlencoded({ extended: false }))
 
 // Route    Get routes/inventory
 // Desc     Return the inventory list
@@ -18,43 +18,75 @@ router.get('/', (req, res) => {
 // posting inventory item to inventory.json
 
 router.post('/', (req, res) => {
-  const inventoryItemDetail =
-  {
-    "id": uuid.v4(),
-    "item": "Product Name Here",
-    "description": "Here is a very brief description of the product…",
-    "lastOrdered": "05/24/2018",
-    "location": "Toronto, CAN",
-    "quantity": "12,000",
-    "status": "In Stock"
+  const inventoryItemDetail = {
+    id: uuid.v4(),
+    item: 'Product Name Here',
+    description: 'Here is a very brief description of the product…',
+    lastOrdered: '05/24/2018',
+    location: 'Toronto, CAN',
+    quantity: '12,000',
+    status: 'In Stock',
   }
 
   inventoryList.push(inventoryItemDetail)
-  fs.writeFile(INVENTORY__DATA__FILE, JSON.stringify(inventoryList), (err) => {
+  fs.writeFile(INVENTORY__DATA__FILE, JSON.stringify(inventoryList), err => {
     if (err) {
-      console.error(err);
-      return;
-    };
-    console.log("Inventory Item has been added");
-  });
+      console.error(err)
+      return
+    }
+    console.log('Inventory Item has been added')
+  })
 
-  if (!inventoryItemDetail.item || !inventoryItemDetail.description || !inventoryItemDetail.lastOrdered || !inventoryItemDetail.location || !inventoryItemDetail.quantity || !inventoryItemDetail.status) {
-    return res.status(400).send("Please fill in the blank");
+  if (
+    !inventoryItemDetail.item ||
+    !inventoryItemDetail.description ||
+    !inventoryItemDetail.lastOrdered ||
+    !inventoryItemDetail.location ||
+    !inventoryItemDetail.quantity ||
+    !inventoryItemDetail.status
+  ) {
+    return res.status(400).send('Please fill in the blank')
   }
   res.json(inventoryList)
-});
+})
 
-
-//Return single inventory item based on /:id 
+//Return single inventory item based on /:id
 
 router.get('/:id', (req, res) => {
   let singleItem = inventoryList.findIndex(item => {
     return item.id === req.params.id
   })
 
-  if (singleItem !== -1) { res.send(inventoryList[singleItem]) }
-  else { res.send("No item found") }
+  if (singleItem !== -1) {
+    res.send(inventoryList[singleItem])
+  } else {
+    res.send('No item found')
+  }
+})
+
+// Route    DELETE routes/inventory/:id
+// Desc     Delete an inventory item
+router.delete('/:id', (req, res) => {
+  const singleItem = inventoryList.findIndex(item => {
+    return item.id === req.params.id
+  })
+  if (singleItem !== -1) {
+    inventoryList.splice(singleItem, 1)
+    fs.writeFileSync(
+      INVENTORY__DATA__FILE,
+      JSON.stringify(inventoryList),
+      err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log('Inventory Item has been DELETED')
+      }
+    )
+    res.send(inventoryList)
+  } else {
+    res.send('No item found')
+  }
 })
 
 module.exports = router
-
